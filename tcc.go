631470af5d5dfc5ad34bf08cdb35b32b7e8504e3
Jianfei Wang <me@thinxer.com>
2015-11-19 18:43:39 +0800

    mmap

--- tcc.go
+++ tcc.go
@@ -9,12 +9,19 @@ import "C"
 import (
 	"errors"
 	"unsafe"
+
+	"github.com/edsrzf/mmap-go"
 )
+
+var ErrCompile = errors.New("compilation error")
+var ErrRelocate = errors.New("relocate error")
+var ErrSymbolNotFound = errors.New("symbol not found")
 
 type State struct {
 	h *C.TCCState
 
 	relocated bool
+	code      mmap.MMap
 }
 
 func New() *State {
@@ -24,31 +31,52 @@ func New() *State {
 
 func (s *State) Release() {
 	C.tcc_delete(s.h)
+	if s.relocated {
+		s.code.Unmap()
+	}
 }
 
 func (s *State) Compile(prog string) error {
 	cprog := C.CString(prog)
 	defer C.free(unsafe.Pointer(cprog))
 	if ret := C.tcc_compile_string(s.h, cprog); ret < 0 {
-		return errors.New("compilation failed")
+		return ErrCompile
 	}
 	return nil
 }
 
+func (s *State) relocate() (err error) {
+	if s.relocated {
+		return nil
+	}
+
+	size := C.tcc_relocate(s.h, unsafe.Pointer(uintptr(0)))
+	if size < 0 {
+		return ErrRelocate
+	}
+	s.code, err = mmap.MapRegion(nil, int(size), mmap.EXEC|mmap.RDWR, mmap.ANON, int64(0))
+	if err != nil {
+		return
+	}
+	if ret := C.tcc_relocate(s.h, unsafe.Pointer(&s.code[0])); ret < 0 {
+		s.code.Unmap()
+		return ErrRelocate
+	}
+	s.relocated = true
+
+	return nil
+}
+
 func (s *State) Symbol(name string) (uintptr, error) {
-	if !s.relocated {
-		// TODO: manually manage memory
-		if ret := C.tcc_relocate(s.h, unsafe.Pointer(uintptr(1))); ret < 0 {
-			return 0, errors.New("relocate error")
-		}
-		s.relocated = true
+	if err := s.relocate(); err != nil {
+		return 0, err
 	}
 
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
 
 	if h := uintptr(unsafe.Pointer(C.tcc_get_symbol(s.h, cname))); h == 0 {
-		return 0, errors.New("not found")
+		return 0, ErrSymbolNotFound
 	} else {
 		return h, nil
 	}
